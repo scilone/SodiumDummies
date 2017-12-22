3ca178fedcbffcfa01b87f646493ae1af7e385b0
Polo <nnobre@ccmbenchmark.com>
2017-12-22 17:56:57 +0100

    Add examples + add method to sign messages + symmetric encryption

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # SodiumDummies
+
+## Authentication
+    
+### Symmetric
+    
+#### Sign
+
+```php
+$sodium = new SodiumDummies();
+ 
+$aliceKeyPair   = $sodium->generateKeyPair('sign');
+// custom key pair > $sodium->generateCustomKeyPair('Alice', 'sign');
+ 
+$aliceSecretKey = $sodium->generateSecretKeyFromKeyPair($aliceKeyPair, 'sign');
+$alicePublicKey = $sodium->generatePublicKeyFromKeyPair($aliceKeyPair, 'sign');
+ 
+$msgSigned  = $sodium->signMessage("It's me, Alice!", $aliceSecretKey);
+$msgDecrypt = $sodium->decryptSignedMessage($msgSigned, $alicePublicKey);
+```
+
+## Encryption
+
+### Symmetric
+
+```php
+$sodium = new SodiumDummies();
+ 
+$nonce = $sodium->getNonce();
+$key   = $sodium->generateKeygen('secretbox');
+//custom key > $sodium->generateCustomKey('Custom', 'secretbox');
+ 
+$secretBox        = $sodium->createSecretBox('This is a secret message', $nonce, $key);
+$secretBoxcontent = $sodium->openSecretBox($secretBox, $nonce, $key);
+```
+
+### Asymmetric
+
+#### Anonymous
+
+```php
+$sodium = new SodiumDummies();
+ 
+$aliceKeyPair   = $sodium->generateKeyPair();
+// custom key pair > $sodium->generateCustomKeyPair('Alice', 'box');
+ 
+$aliceSecretKey = $sodium->generateSecretKeyFromKeyPair($aliceKeyPair);
+$alicePublicKey = $sodium->generatePublicKeyFromKeyPair($aliceKeyPair);
+ 
+$anonymousMessageToAlice = $sodium->createBoxSeal('Anonymous messagsse', $alicePublicKey);
+$decryptedMessage        = $sodium->openBoxSeal($anonymous_message_to_alice, $aliceKeyPair);
+```
+
+#### Authenticated
+
+```php
+$sodium = new SodiumDummies();
+ 
+$aliceKeyPair   = $sodium->generateKeyPair();
+// custom key pair > $sodium->generateCustomKeyPair('Alice', 'box');
+ 
+$aliceSecretKey = $sodium->generateSecretKeyFromKeyPair($aliceKeyPair);
+$alicePublicKey = $sodium->generatePublicKeyFromKeyPair($aliceKeyPair);
+ 
+$bobKeyPair   = $sodium->generateKeyPair();
+// custom key pair > $sodium->generateCustomKeyPair('Bob', 'box');
+ 
+$bobSecretKey = $sodium->generateSecretKeyFromKeyPair($bobKeyPair);
+$bobPublicKey = $sodium->generatePublicKeyFromKeyPair($bobKeyPair);
+ 
+$aliceToBobKey = $sodium->getKeyFromSecretKeyAndPublicKey($aliceSecretKey, $bobPublicKey);
+$bobToAliceKey = $sodium->getKeyFromSecretKeyAndPublicKey($bobSecretKey, $alicePublicKey);
+ 
+$nonce = $sodium->getNonce();
+ 
+$messageCrypted          = $sodium->createBox("Hello Bob/Alice", $nonce, $aliceToBobKey);
+$messageDecryptedByBob   = $sodium->openBox($messageCrypted, $nonce, $bobToAliceKey);
+$messageDecryptedByAlice = $sodium->openBox($messageCrypted, $nonce, $aliceToBobKey);
+```
--- SodiumDummies.php
+++ SodiumDummies.php
@@ -26,7 +26,40 @@ class SodiumDummies
                 return sodium_crypto_box_keypair();
             default:
                 throw new Exception('Unknown type');
+        }
+    }
 
+    /**
+     * @param string $type
+     *
+     * @throws Exception
+     *
+     * @return string
+     */
+    public function generateKeygen(string $type = '') :string
+    {
+        switch ($type) {
+            case 'aes256gcm':
+                return sodium_crypto_aead_aes256gcm_keygen();
+            case 'chacha20poly1305_ietf':
+                return sodium_crypto_aead_chacha20poly1305_ietf_keygen();
+            case 'chacha20poly1305':
+                return sodium_crypto_aead_chacha20poly1305_keygen();
+            case 'auth':
+                return sodium_crypto_auth_keygen();
+            case 'kdf':
+                return sodium_crypto_kdf_keygen();
+            case 'secretbox':
+                return sodium_crypto_secretbox_keygen();
+            case 'shorthash':
+                return sodium_crypto_shorthash_keygen();
+            case 'stream':
+                return sodium_crypto_stream_keygen();
+            case '':
+            case 'generichash':
+                return sodium_crypto_generichash_keygen();
+            default:
+                throw new Exception('Unknown type');
         }
     }
 
@@ -45,33 +78,73 @@ class SodiumDummies
         switch ($type) {
             case 'kx':
                 return sodium_crypto_kx_seed_keypair(
-                    sodium_crypto_generichash(
-                        $text,
-                        '',
-                        SODIUM_CRYPTO_KX_BYTES
-                    )
+                    $this->generateCustomKey($text)
                 );
             case 'sign':
                 return sodium_crypto_sign_seed_keypair(
-                    sodium_crypto_generichash(
-                        $text,
-                        '',
-                        SODIUM_CRYPTO_SIGN_BYTES
-                    )
+                    $this->generateCustomKey($text, 'sign')
                 );
             case 'box':
             case '':
                 return sodium_crypto_box_seed_keypair(
-                    sodium_crypto_generichash(
-                        $text,
-                        '',
-                        SODIUM_CRYPTO_BOX_SEEDBYTES
-                    )
+                    $this->generateCustomKey($text)
                 );
             default:
                 throw new Exception('Unknown type');
-
         }
+    }
+
+    /**
+     * @param string $text
+     * @param string $type
+     *
+     * @throws Exception
+     *
+     * @return string
+     */
+    public function generateCustomKey(string $text, string $type = '') :string
+    {
+        switch ($type) {
+            case 'auth':
+                $lenght = SODIUM_CRYPTO_AUTH_KEYBYTES;
+                break;
+            case 'secretbox':
+                $lenght = SODIUM_CRYPTO_SECRETBOX_KEYBYTES;
+                break;
+            case 'shorthash':
+                $lenght = SODIUM_CRYPTO_SHORTHASH_KEYBYTES;
+                break;
+            case 'stream':
+                $lenght = SODIUM_CRYPTO_STREAM_KEYBYTES;
+                break;
+            case 'max':
+                $lenght = SODIUM_CRYPTO_GENERICHASH_KEYBYTES_MAX;
+                break;
+            case 'sign':
+                $lenght = SODIUM_CRYPTO_SIGN_SEEDBYTES;
+                break;
+            case 'min':
+                $lenght = SODIUM_CRYPTO_GENERICHASH_KEYBYTES_MIN;
+                break;
+            case 'aes256gcm':
+                $lenght = SODIUM_CRYPTO_AEAD_AES256GCM_KEYBYTES;
+                break;
+            case 'secretbox':
+                $lenght = SODIUM_CRYPTO_SECRETBOX_KEYBYTES;
+                break;
+            case 'generichash':
+            case '':
+                $lenght = SODIUM_CRYPTO_GENERICHASH_KEYBYTES;
+                break;
+            default:
+                throw new Exception('Unknown type');
+        }
+
+        return sodium_crypto_generichash(
+            $text,
+            '',
+            $lenght
+        );
     }
 
     /**
@@ -96,7 +169,6 @@ class SodiumDummies
                 return sodium_crypto_box_secretkey($keyPair);
             default:
                 throw new Exception('Unknown type');
-
         }
     }
 
@@ -122,7 +194,6 @@ class SodiumDummies
                 return sodium_crypto_box_publickey($keyPair);
             default:
                 throw new Exception('Unknown type');
-
         }
     }
 
@@ -148,7 +219,6 @@ class SodiumDummies
                 return sodium_crypto_box_keypair_from_secretkey_and_publickey($secretKey, $publicKey);
             default:
                 throw new Exception('Unknown type');
-
         }
     }
 
@@ -171,7 +241,6 @@ class SodiumDummies
                 return random_bytes(SODIUM_CRYPTO_BOX_NONCEBYTES);
             default:
                 throw new Exception('Unknown type');
-
         }
     }
 
@@ -233,5 +302,64 @@ class SodiumDummies
     public function openBoxSeal(string $textCrypted, string $privateKey) :string
     {
         return sodium_crypto_box_seal_open($textCrypted, $privateKey);
+    }
+
+    /**
+     * Create a secret box
+     * A secret box crypt a message with a key, only this key can decrypt this message
+     * So all people who want open the secret box must know the "master" key
+     *
+     * @param string $text
+     * @param string $nonce
+     * @param string $key
+     *
+     * @return string
+     */
+    public function createSecretBox(string $text, string $nonce, string $key) :string
+    {
+        return sodium_crypto_secretbox($text, $nonce, $key);
+    }
+
+    /**
+     * Open a secret box
+     * Everyone who know the key can open it
+     *
+     * @param string $textCrypted
+     * @param string $nonce
+     * @param string $key
+     *
+     * @return string
+     */
+    public function openSecretBox(string $textCrypted, string $nonce, string $key) :string
+    {
+        return sodium_crypto_secretbox_open($textCrypted, $nonce, $key);
+    }
+
+    /**
+     * Sign a message
+     * A signed message can only be decrypted with the public key of the person who sign it
+     *
+     * @param string $message
+     * @param string $secretKey
+     *
+     * @return string
+     */
+    public function signMessage(string $message, string $secretKey) :string
+    {
+        return sodium_crypto_sign($message, $secretKey);
+    }
+
+    /**
+     * Decrypt a signed message
+     * If the message is correctly decrypted, that prove that the message was write by the public key owner
+     *
+     * @param string $messageSigned
+     * @param string $publicKey
+     *
+     * @return string
+     */
+    public function decryptSignedMessage(string $messageSigned, string $publicKey) :string
+    {
+        return sodium_crypto_sign_open($messageSigned, $publicKey);
     }
 }
